bd7d58d367710fba31a92fa529eb5fd71f747adb
Robert Good <rtgood@ucdavis.edu>
2025-09-12 16:31:34 -0700

    U

--- ScriptLanguageTesting/ScriptLanguageTesting.cpp
+++ ScriptLanguageTesting/ScriptLanguageTesting.cpp
@@ -694,7 +694,21 @@ int main() {
             }
             EXPECT_EQ(1000000, out);
         }
-
+        if (auto timer = sw.debug_timer("Parallel Jobs Test 3")) {
+            size_t out = 0;
+            {
+                std::vector<size_t> jobs;
+                jobs.resize(1000000, 0);
+                GL::parallel::async(0, 1000000, [&](size_t i) {
+                    EXPECT_EQ(1000000, jobs.size());
+                    auto start = GL::clock::ns();
+                    while ((GL::clock::ns() - start) < 1000) {}
+                    ++jobs[i];
+                });
+                out = std::accumulate(jobs.begin(), jobs.end(), 0);
+            }
+            EXPECT_EQ(1000000, out);
+        }
 
 
 
